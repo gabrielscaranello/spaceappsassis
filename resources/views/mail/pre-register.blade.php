c4b3ef0a22b843b9ef3d226cda0c30305f41aa1c
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2019-08-26 19:39:56 -0300

    register mail template

---
+++ resources/views/mail/pre-register.blade.php
@@ -0,0 +1,31 @@
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        font-family: roboto, sans-serif;
+    }
+</style>
+<div style="width: 100%; background: #000; position: relative; padding-bottom: 1rem; padding-top: 1rem;">
+    <div style="text-align: center;">
+        <h4 style="color: #fff; margin-bottom: 1.25rem;">
+            NASA SPACE APPS ASSIS CHATEAUBRIAND 2019
+        </h4>
+    </div>
+
+    <div style="text-align: justify; background: #fff; border-radius: .5rem; overflow: hidden; padding: 1rem; display: block; margin: 1rem;">
+        <h4 style="margin-bottom: 1rem;">
+            Olá {{$data['first_name']}},
+        </h4>
+        <p>
+            Sua pré-inscrição no Space Apps Assis Chateaubriand 2019 foi realizada com sucesso.
+        </p>
+        <p>
+            Aguarde, em breve abriremos as inscrições oficiais.
+        </p>
+    </div>
+
+    <div style="text-align: center;">
+        <img src="img/logo.png" style="width: 7rem;display: block;margin-left: auto;margin-right: auto;margin-bottom: 1rem;">
+        <a target="_blank" href="https://spaceappsassis.com" style="color: #fff; text-decoration: none; font-weight: bold;">spaceappsassis.com</a>
+    </div>
+</div>
